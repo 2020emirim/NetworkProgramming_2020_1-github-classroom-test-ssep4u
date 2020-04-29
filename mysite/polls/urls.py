3ae9af5849978ad230cc8988e03c961c318233ae
ssep4u <ssep4u@gmail.com>
2020-04-29 09:57:07 +0900

    polls/views, urls detail, vote, results

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # 127.0.0.1:8000/polls/
     path('', views.index, name='index'),
+    # 127.0.0.1:8000/polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # 127.0.0.1:8000/polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # 127.0.0.1:8000/polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
