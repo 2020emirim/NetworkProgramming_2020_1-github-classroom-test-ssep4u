b0dd425d9fe14aad0ffe6723aba3e928e6c88c15
ssep4u <ssep4u@gmail.com>
2020-05-07 09:21:13 +0900

    polls/urls, views generic(Class-based view)

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,11 +5,11 @@ app_name = 'polls'
 
 urlpatterns = [
     # 127.0.0.1:8000/polls/
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     # 127.0.0.1:8000/polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # 127.0.0.1:8000/polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # 127.0.0.1:8000/polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
